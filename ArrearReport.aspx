a870b3565db581dae4e4de1faaebe39342647977
Rohitkumar68 <82360549+Rohitkumar68@users.noreply.github.com>
2021-04-14 11:40:30 +0530

    Add files via upload
    First commit

---
+++ ArrearReport.aspx
@@ -0,0 +1,147 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage/SalaryMaster.master" AutoEventWireup="true" CodeFile="ArrearReport.aspx.cs" Inherits="SalaryModule_ArrearReport" EnableEventValidation="false" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <script language="javascript" type="text/javascript">
+
+        function ConfirmDeactivate() {
+            if (confirm("Are you sure you want to Deactivate this Record?") == true)
+                return true;
+            else
+                return false;
+        }
+
+    </script>
+    <asp:UpdateProgress ID="MyProcess" runat="server" AssociatedUpdatePanelID="UpdatePanel1"
+        DisplayAfter="5">
+        <ProgressTemplate>
+            <div style="left: 0; position: fixed; width: 100%; height: 100%; z-index: 9999999; top: 0; background: rgba(0,0,0,0.5);">
+                <div style="text-align: center; z-index: 10; margin: 300px auto;">
+                    <img alt="img" src="../Images/loading-gif-animation.gif" style="height: 100px; width: 100px;" /><br />
+                    <br />
+                    <span>
+                        <h4>
+                            <asp:Label runat="server" Text="Please Wait..." ID="lblPleaseWait"></asp:Label>
+                    </span>
+                    </h4>
+                </div>
+            </div>
+        </ProgressTemplate>
+    </asp:UpdateProgress>
+
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div style="margin-top: 25px; height: auto; width: 100%; overflow: auto;">
+                <fieldset>
+                    <center>
+                        <table style="margin: 30px 0 0 0;">
+                            <tr>
+                                <td>
+                                    <asp:Label ID="lblReport" runat="server" Font-Bold="true" Font-Size="15px" Text="Arrear Report"></asp:Label>
+                                </td>
+                                <td style="width: 700px;"></td>
+                                <asp:Panel ID="pnlTotalRecords" runat="server" Visible="false">
+                                    <td>
+                                        <asp:Label ID="lblTotal" runat="server" Font-Bold="true" ForeColor="Red" Font-Size="15px"
+                                            Text="Total Records : "></asp:Label>
+                                    </td>
+                                    <td>
+                                        <asp:Label ID="lblTotalRecords" runat="server" Font-Bold="true" ForeColor="Red"
+                                            Font-Size="15px"></asp:Label>
+                                    </td>
+                                </asp:Panel>
+                            </tr>
+                        </table>
+                    </center>
+                    <asp:Panel ID="pnlData" runat="server">
+                        <center>
+                            <table style="margin: 15px 0 0 21px;">
+                                <tr>
+                                    <td>
+                                        <asp:DropDownList ID="ddlReportType" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlReportType_SelectedIndexChanged" Visible="false">
+                                            <asp:ListItem Text="Select Report Type" Value="0"></asp:ListItem>
+                                            <asp:ListItem Text="Monthwise Report" Value="1" Selected="True"></asp:ListItem>
+                                            <asp:ListItem Text="Individual Employee Report" Value="2"></asp:ListItem>
+                                        </asp:DropDownList>
+                                        <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="ddlReportType" ErrorMessage="*" ForeColor="Red" InitialValue="0" ValidationGroup="Search"></asp:RequiredFieldValidator>
+                                    </td>
+                                    <td>
+                                        <asp:DropDownList ID="ddlMonth" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlMonth_SelectedIndexChanged"></asp:DropDownList>
+                                        <asp:RequiredFieldValidator ID="RequiredFieldValidator14" runat="server" ControlToValidate="ddlMonth" ErrorMessage="*" ForeColor="Red" InitialValue="0" ValidationGroup="Search"></asp:RequiredFieldValidator>
+                                    </td>
+                                    <td>
+                                        <asp:DropDownList ID="ddlYear" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlYear_SelectedIndexChanged"></asp:DropDownList>
+                                        <asp:RequiredFieldValidator ID="RequiredFieldValidator13" runat="server" ControlToValidate="ddlYear" ErrorMessage="*" InitialValue="0" ForeColor="Red" ValidationGroup="Search"></asp:RequiredFieldValidator>
+                                    </td>
+                                    <asp:Panel ID="pnlEmployees" runat="server" Visible="false">
+                                        <td>
+                                            <asp:DropDownList ID="ddlemployee" runat="server"></asp:DropDownList>
+                                            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="ddlemployee" ErrorMessage="*" InitialValue="0" ForeColor="Red" ValidationGroup="Search"></asp:RequiredFieldValidator>
+                                        </td>
+                                    </asp:Panel>
+                                    <td>
+                                        <asp:Button ID="btnSearch" runat="server" Text="Get Arrear Report" CssClass="btn btn-default" ValidationGroup="Search" OnClick="btnSearch_Click" />
+                                    </td>
+                                    <td>
+                                        <asp:Button ID="btnCancel" Text="Cancel" runat="server" CssClass="btn btn-primary" OnClick="btnCancel_Click" />
+                                    </td>
+                                    <td>
+                                        <asp:LinkButton ID="lnkExportToExcel" runat="server" Visible="false" OnClick="lnkExportToExcel_Click">
+                                            <asp:Image ID="ImgExportToExcel" runat="server" Height="35px" Width="35px" ToolTip="Export To Excel" ImageUrl="~/images/ExportToExcel.png" />
+                                        </asp:LinkButton>
+                                    </td>
+                                </tr>
+                            </table>
+                        </center>
+                    </asp:Panel>
+                </fieldset>
+            </div>
+            <style type="text/css">
+                .grdTable {
+                    max-width: 200% !Important;
+                    min-width: 200%;
+                    overflow: auto;
+                }
+            </style>
+            <center>
+                <asp:Panel ID="pnlStmt" runat="server" Style="margin-top: 30px;">
+                    <asp:Label ID="lblSTMT" runat="server" Font-Size="Medium" Font-Bold="true"></asp:Label>
+                </asp:Panel>
+            </center>  
+            <asp:Panel ID="pnlMonthlyReport" runat="server" Style="margin-top: 30px; max-height: 550px; width: 100%; overflow: auto; margin-bottom: 20px;" Visible="false">
+                <asp:GridView ID="grdMonthlyReport" runat="server" AutoGenerateColumns="false" Width="100%" OnRowCommand="grdMonthlyReport_RowCommand">
+                    <HeaderStyle HorizontalAlign="Center" Font-Bold="True" ForeColor="Black" Height="50px" />
+                    <RowStyle HorizontalAlign="Center" Height="50px" />
+                    <EmptyDataRowStyle ForeColor="Red" HorizontalAlign="Center" />
+                    <FooterStyle Font-Bold="true" HorizontalAlign="Center" Height="50px" />
+                    <Columns>
+                        <asp:TemplateField HeaderText="S.No">
+                            <ItemTemplate>
+                                <%#Container.DataItemIndex+1 %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:BoundField HeaderText="Emp Code" DataField="Emp_Code" />
+                        <asp:BoundField HeaderText="Name" DataField="Name" />
+                        <asp:BoundField HeaderText="Des" DataField="Designation" />
+                        <asp:BoundField HeaderText="Arrear Amount" DataField="ArearValue" />
+                        <asp:BoundField HeaderText="Remarks" DataField="Remarks" />
+                        <asp:TemplateField HeaderText="Action">
+                            <ItemTemplate>
+                                <asp:LinkButton ID="lnkDeactivate" runat="server" Text="Deactivate" OnClientClick="return ConfirmDeactivate()" CommandName="lnkDeactivate" CommandArgument='<%#Eval("ArearImportID") %>' />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                    <EmptyDataTemplate>
+                        No Record Found
+                    </EmptyDataTemplate>
+                </asp:GridView>
+            </asp:Panel>
+            <div style="min-height: 350px;"></div>
+        </ContentTemplate>
+        <Triggers>
+            <asp:PostBackTrigger ControlID="lnkExportToExcel" />
+        </Triggers>
+    </asp:UpdatePanel>
+</asp:Content>
+
---
+++ ArrearReport.aspx.cs
@@ -0,0 +1,483 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+public partial class SalaryModule_ArrearReport : System.Web.UI.Page
+{
+    string constr = "";
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    Hashtable hash;
+    decimal ExtraPaidDaysSalary = 0;
+
+    protected void page_Init()
+    {
+        constr = ConfigurationManager.ConnectionStrings["myconnectionstring"].ConnectionString;
+        SqlConnection con = new SqlConnection(constr);
+        da = new SqlDataAdapter();
+        cmd = new SqlCommand();
+        hash = new Hashtable();
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (Session["User"] != null)
+            {
+                hash = (Hashtable)Session["User"];
+                if (!IsPostBack)
+                {
+                    CheckUserRights();
+                }
+            }
+            else
+            {
+                Response.Redirect("../Default.aspx", false);
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public enum MenuType
+    {
+        All = 0,
+        SetUp = 1,
+        ImportData = 2,
+        Actions = 3,
+        Reports = 4
+    }
+
+    public enum IsApprove
+    {
+        All = 0,
+        Approve = 1,
+        NotApprove = 2
+    }
+
+    public enum SalaryStatus
+    {
+        All = 0,
+        Release = 1,
+        Hold = 2
+    }
+
+    public enum Status
+    {
+        Active = 1,
+        Deactive = 0
+    }
+
+    public void CheckUserRights()
+    {
+        try
+        {
+            int HasMatch = 0;
+            string RequestURL = Request.Url.AbsolutePath;
+            System.IO.FileInfo oInfo = new System.IO.FileInfo(RequestURL);
+            string PageName = oInfo.Name;
+            string CheckPageName = "";
+
+            SqlConnection con = new SqlConnection(constr);
+            cmd = new SqlCommand("GetLoginDetails", con);
+            cmd.Parameters.AddWithValue("@UserName", null);
+            cmd.Parameters.AddWithValue("@Password", null);
+            cmd.Parameters.AddWithValue("@LoginID", Session["LoginID"]);
+            cmd.Parameters.AddWithValue("@MenuID", MenuType.All);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            DataSet ds = new DataSet();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            con.Close();
+            if (ds.Tables[1].Rows.Count > 0)
+            {
+                int i = 0;
+
+                foreach (DataRow row in ds.Tables[1].Rows)
+                {
+                    CheckPageName = ds.Tables[1].Rows[i]["PageName"].ToString();
+                    if (PageName == CheckPageName)
+                    {
+                        HasMatch++;
+                        break;
+                    }
+
+                    i++;
+                }
+
+                if (HasMatch > 0)
+                {
+                    Month();
+                    Year();
+                }
+                else
+                {
+                    Response.Redirect("../NotAuthorized/NotAuthorized.aspx");
+                }
+            }
+            else
+            {
+                Month();
+                Year();
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public void Month()
+    {
+        try
+        {
+            SqlConnection con = new SqlConnection(constr);
+            cmd = new SqlCommand("ShowMonth", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            ddlMonth.DataSource = dt;
+            ddlMonth.DataTextField = "MonthName";
+            ddlMonth.DataValueField = "MonthID";
+            ddlMonth.DataBind();
+            ddlMonth.Items.Insert(0, new ListItem("Select Month", "0"));
+            con.Close();
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public void Year()
+    {
+        try
+        {
+            SqlConnection con = new SqlConnection(constr);
+            cmd = new SqlCommand("ManageYears", con);
+            cmd.Parameters.AddWithValue("@Year", null);
+            cmd.Parameters.AddWithValue("@User", null);
+            cmd.Parameters.AddWithValue("@Type", "GetRecords");
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            ddlYear.DataSource = dt;
+            ddlYear.DataTextField = "Year";
+            ddlYear.DataValueField = "YearID";
+            ddlYear.DataBind();
+            ddlYear.Items.Insert(0, new ListItem("Select Year", "0"));
+            con.Close();
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public void Employee()
+    {
+        try
+        {
+            SqlConnection con = new SqlConnection(constr);
+            cmd = new SqlCommand("ShowArrearDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@MonthID", ddlMonth.SelectedValue);
+            cmd.Parameters.AddWithValue("@YearID", ddlYear.SelectedValue);
+            con.Open();
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            DataView dv = new DataView(dt);
+            dv.Sort = "Name ASC";
+            ddlemployee.DataSource = dv;
+            ddlemployee.DataTextField = "DropText";
+            ddlemployee.DataValueField = "ProfileID";
+            ddlemployee.DataBind();
+            ddlemployee.Items.Insert(0, new ListItem("All Employees", "0"));
+            con.Close();
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public void BindMonthlygrid()
+    {
+        try
+        {
+            SqlConnection con = new SqlConnection(constr);
+            cmd = new SqlCommand("ShowArrearDetails", con);
+            cmd.Parameters.AddWithValue("@MonthID", ddlMonth.SelectedValue);
+            cmd.Parameters.AddWithValue("@YearID", ddlYear.SelectedValue);
+            cmd.Parameters.AddWithValue("@ReportType", ddlReportType.SelectedValue);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            con.Close();
+
+            if (dt.Rows.Count > 0)
+            {
+                pnlMonthlyReport.Visible = true;
+                ddlReportType.Enabled = false;
+                ddlMonth.Enabled = false;
+                ddlYear.Enabled = false;
+                ddlemployee.Enabled = false;
+                lnkExportToExcel.Visible = true;
+                lblSTMT.Text = "G. D. Enterprise " + "<br />" + "Arrear Report For the Month of " + ddlMonth.SelectedItem.Text + " - " + ddlYear.SelectedItem.Text;
+            }
+            else
+            {
+                pnlMonthlyReport.Visible = false;
+                ddlReportType.Enabled = true;
+                ddlMonth.Enabled = true;
+                ddlYear.Enabled = true;
+                ddlemployee.Enabled = true;
+                lnkExportToExcel.Visible = false;
+                lblSTMT.Text = string.Empty;
+            }
+            grdMonthlyReport.DataSource = dt;
+            grdMonthlyReport.DataBind();
+            pnlTotalRecords.Visible = true;
+            lblTotalRecords.Text = dt.Rows.Count.ToString();
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public void Clear()
+    {
+        try
+        {
+            pnlTotalRecords.Visible = false;
+            lblTotalRecords.Text = string.Empty;
+            ddlMonth.ClearSelection();
+            ddlYear.ClearSelection();
+            ddlemployee.ClearSelection();
+            ddlReportType.SelectedValue = "1";
+            ddlMonth.Enabled = true;
+            ddlYear.Enabled = true;
+            ddlemployee.Enabled = true;
+            ddlReportType.Enabled = true;
+            pnlEmployees.Visible = false;
+            grdMonthlyReport.DataSource = null;
+            grdMonthlyReport.DataBind();
+            pnlMonthlyReport.Visible = false;
+            lnkExportToExcel.Visible = false;
+            lblSTMT.Text = string.Empty;
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    protected void ddlMonth_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            if (Convert.ToInt32(ddlMonth.SelectedValue) > 0 && Convert.ToInt32(ddlYear.SelectedValue) > 0 && Convert.ToInt32(ddlReportType.SelectedValue) == 2)
+            {
+                pnlEmployees.Visible = true;
+                Employee();
+            }
+            else
+            {
+                pnlEmployees.Visible = false;
+                ddlemployee.ClearSelection();
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    protected void ddlYear_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            if (Convert.ToInt32(ddlMonth.SelectedValue) > 0 && Convert.ToInt32(ddlYear.SelectedValue) > 0 && Convert.ToInt32(ddlReportType.SelectedValue) == 2)
+            {
+                pnlEmployees.Visible = true;
+                Employee();
+            }
+            else
+            {
+                pnlEmployees.Visible = false;
+                ddlemployee.ClearSelection();
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (Convert.ToInt32(ddlReportType.SelectedValue) == 1)
+            {
+                BindMonthlygrid();
+            }
+            else if (Convert.ToInt32(ddlReportType.SelectedValue) == 2)
+            {
+
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Clear();
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    protected void ddlReportType_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            if (Convert.ToInt32(ddlMonth.SelectedValue) > 0 && Convert.ToInt32(ddlYear.SelectedValue) > 0 && Convert.ToInt32(ddlReportType.SelectedValue) == 2)
+            {
+                pnlEmployees.Visible = true;
+                Employee();
+            }
+            else
+            {
+                pnlEmployees.Visible = false;
+                ddlemployee.ClearSelection();
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    protected void grdMonthlyReport_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        try
+        {
+            if (e.CommandName == "lnkDeactivate")
+            {
+                hash = new Hashtable();
+                hash = (Hashtable)Session["User"];
+                SqlConnection con = new SqlConnection(constr);
+                cmd = new SqlCommand("SaveArrearDetails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@MonthID", ddlMonth.SelectedValue);
+                cmd.Parameters.AddWithValue("@YearID", ddlYear.SelectedValue);
+                cmd.Parameters.AddWithValue("@ProfileID", ddlemployee.SelectedValue);
+                cmd.Parameters.AddWithValue("@Emp_Code", null);
+                cmd.Parameters.AddWithValue("@SystemNumber", null);
+                cmd.Parameters.AddWithValue("@AssignEmpCode", null);
+                cmd.Parameters.AddWithValue("@Name", null);
+                cmd.Parameters.AddWithValue("@HRAApply", null);
+                cmd.Parameters.AddWithValue("@ModeChangeRemarks", null);
+                cmd.Parameters.AddWithValue("@User", Convert.ToString(hash["Name"].ToString()));
+                cmd.Parameters.AddWithValue("@Type", "Deactivate");
+                cmd.Parameters.AddWithValue("@ArrearID", e.CommandArgument);
+                con.Open();
+                int Count = cmd.ExecuteNonQuery();
+                con.Close();
+
+                if (Count > 0)
+                {
+                    BindMonthlygrid();
+                    ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('Record Deactivated Sucessfully.');", true);
+                }
+                else
+                {
+                    ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('Failed to Perform Action.');", true);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    private void ExportGridToExcel()
+    {
+        try
+        {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment;filename=ArrearReport.xls");
+            Response.Charset = "";
+            Response.ContentType = "application/vnd.ms-excel";
+            using (StringWriter sw = new StringWriter())
+            {
+                HtmlTextWriter hw = new HtmlTextWriter(sw);
+                grdMonthlyReport.AllowPaging = false;
+                this.BindMonthlygrid();
+                foreach (GridViewRow row in grdMonthlyReport.Rows)
+                {
+                    grdMonthlyReport.Columns[6].Visible = false;
+                }
+                grdMonthlyReport.Caption = lblSTMT.Text;
+                grdMonthlyReport.RenderControl(hw);
+                string style = @"<style> .textmode { } </style>";
+                Response.Write(style);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    protected void lnkExportToExcel_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            ExportGridToExcel();
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public override void VerifyRenderingInServerForm(Control control)
+    {
+        /* Verifies that the control is rendered */
+    }
+}
