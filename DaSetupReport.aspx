e653b257f9238ba3785cd9f593cb0be8a2dfab10
Rohitkumar68 <82360549+Rohitkumar68@users.noreply.github.com>
2021-04-14 12:06:33 +0530

    Add files via upload
    DA SETUP

---
+++ DaSetupReport.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage/SalaryMaster.master" AutoEventWireup="true" CodeFile="DaSetupReport.aspx.cs" Inherits="SalaryModule_DaSetupReport" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <asp:UpdateProgress ID="MyProcess" runat="server" AssociatedUpdatePanelID="UpdatePanel1"
+        DisplayAfter="5">
+        <ProgressTemplate>
+            <div style="left: 0; position: fixed; width: 100%; height: 100%; z-index: 9999999; top: 0; background: rgba(0,0,0,0.5);">
+                <div style="text-align: center; z-index: 10; margin: 300px auto;">
+                    <img alt="img" src="../Images/loading-gif-animation.gif" style="height: 100px; width: 100px;" /><br />
+                    <br />
+                    <span>
+                        <h4>
+                            <asp:Label runat="server" Text="Please Wait..." ID="lblPleaseWait"></asp:Label>
+                    </span>
+                    </h4>
+                </div>
+            </div>
+        </ProgressTemplate>
+    </asp:UpdateProgress>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div style="margin-top: 25px; height: auto;">
+                <fieldset>
+                    <center>
+                        <table>
+                            <tr>
+                                <td>
+                                    <legend>Dearness Allowance (DA) Set Up Report</legend>
+                                </td>
+                                <td>
+                                    <asp:LinkButton ID="lnkExportToExcel" runat="server" Visible="false" OnClick="lnkExportToExcel_Click">
+                                        <asp:Image ID="ImgExportToExcel" runat="server" Height="35px" Width="35px" ToolTip="Export To Excel" ImageUrl="~/images/ExportToExcel.png" />
+                                    </asp:LinkButton>
+                                </td>
+                            </tr>
+                        </table>
+                    </center>
+                    <asp:Panel ID="pnlDetail" runat="server" Style="margin-top: 30px; max-height: 500px; width: 100%; overflow: auto; margin-bottom: 20px;">
+
+                        <asp:GridView ID="grdrecord" runat="server" AutoGenerateColumns="false" Width="100%">
+                            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                            <HeaderStyle BackColor="#C8C6C6" Font-Bold="True" ForeColor="Black" />
+                            <EditRowStyle BackColor="#999999" />
+                            <AlternatingRowStyle BackColor="#E2DED6" ForeColor="#284775" />
+                            <RowStyle HorizontalAlign="Center" BackColor="#F7F6F3" ForeColor="#284775" />
+                            <EmptyDataRowStyle ForeColor="Red" HorizontalAlign="Center" />
+                            <Columns>
+                                <asp:TemplateField HeaderText="S.No.">
+                                    <ItemTemplate>
+                                        <%#Container.DataItemIndex+1 %>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:BoundField HeaderText="DA (%)" DataField="Da" />
+                                <asp:BoundField HeaderText="Created By" DataField="CreatedBy" />
+                                <asp:BoundField HeaderText="Created On" DataField="CreatedOn" />
+                                <asp:BoundField HeaderText="Updated By" DataField="UpdatedBy" />
+                                <asp:BoundField HeaderText="Updated On" DataField="UpdatedOn" />
+                            </Columns>
+                            <EmptyDataTemplate>
+                                No Record Found
+                            </EmptyDataTemplate>
+                        </asp:GridView>
+                    </asp:Panel>
+                </fieldset>
+            </div>
+            <div style="min-height: 370px;"></div>
+        </ContentTemplate>
+        <Triggers>
+            <asp:PostBackTrigger ControlID="lnkExportToExcel" />
+        </Triggers>
+    </asp:UpdatePanel>
+</asp:Content>
+
---
+++ DaSetupReport.aspx.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Drawing;
+
+public partial class SalaryModule_DaSetupReport : System.Web.UI.Page
+{
+    string constr = "";
+    SqlConnection con;
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    Hashtable hash;
+
+    protected void page_Init()
+    {
+        constr = ConfigurationManager.ConnectionStrings["myconnectionstring"].ConnectionString;
+        SqlConnection con = new SqlConnection(constr);
+        da = new SqlDataAdapter();
+        cmd = new SqlCommand();
+        hash = new Hashtable();
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (Session["User"] != null)
+            {
+                hash = (Hashtable)Session["User"];
+                if (!IsPostBack)
+                {
+                    CheckUserRights();
+                }
+            }
+            else
+            {
+                Response.Redirect("../Default.aspx", false);
+            }
+        }
+        catch (Exception ex)
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            String str = ser.Serialize(ex.Message);
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", "<script>alert('" + str + "');</script>");
+        }
+    }
+
+    public enum MenuType
+    {
+        All = 0,
+        SetUp = 1,
+        ImportData = 2,
+        Actions = 3,
+        Reports = 4
+    }
+
+    public void CheckUserRights()
+    {
+        try
+        {
+            int HasMatch = 0;
+            string RequestURL = Request.Url.AbsolutePath;
+            System.IO.FileInfo oInfo = new System.IO.FileInfo(RequestURL);
+            string PageName = oInfo.Name;
+            string CheckPageName = "";
+
+            SqlConnection con = new SqlConnection(constr);
+            cmd = new SqlCommand("GetLoginDetails", con);
+            cmd.Parameters.AddWithValue("@UserName", null);
+            cmd.Parameters.AddWithValue("@Password", null);
+            cmd.Parameters.AddWithValue("@LoginID", Session["LoginID"]);
+            cmd.Parameters.AddWithValue("@MenuID", MenuType.All);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            DataSet ds = new DataSet();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            con.Close();
+            if (ds.Tables[1].Rows.Count > 0)
+            {
+                int i = 0;
+
+                foreach (DataRow row in ds.Tables[1].Rows)
+                {
+                    CheckPageName = ds.Tables[1].Rows[i]["PageName"].ToString();
+                    if (PageName == CheckPageName)
+                    {
+                        HasMatch++;
+                        break;
+                    }
+
+                    i++;
+                }
+
+                if (HasMatch > 0)
+                {
+                    Bindgrid();
+                }
+                else
+                {
+                    Response.Redirect("../NotAuthorized/NotAuthorized.aspx");
+                }
+            }
+            else
+            {
+                Bindgrid();
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public void Bindgrid()
+    {
+        try
+        {
+            SqlConnection con = new SqlConnection(constr);
+            cmd = new SqlCommand("ShowDaSetup", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            con.Close();
+            if (dt.Rows.Count > 0)
+            {
+                lnkExportToExcel.Visible = true;
+            }
+            else
+            {
+                lnkExportToExcel.Visible = false;
+            }
+            grdrecord.DataSource = dt;
+            grdrecord.DataBind();
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    private void ExportGridToExcel()
+    {
+        try
+        {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment;filename=DA Setup Report.xls");
+            Response.Charset = "";
+            Response.ContentType = "application/vnd.ms-excel";
+            using (StringWriter sw = new StringWriter())
+            {
+                HtmlTextWriter hw = new HtmlTextWriter(sw);
+
+                //To Export all pages
+                grdrecord.AllowPaging = false;
+                this.Bindgrid();
+
+                grdrecord.HeaderRow.BackColor = Color.White;
+                foreach (TableCell cell in grdrecord.HeaderRow.Cells)
+                {
+                    cell.BackColor = grdrecord.HeaderStyle.BackColor;
+                }
+                foreach (GridViewRow row in grdrecord.Rows)
+                {
+                    row.BackColor = Color.White;
+                    foreach (TableCell cell in row.Cells)
+                    {
+                        if (row.RowIndex % 2 == 0)
+                        {
+                            cell.BackColor = grdrecord.AlternatingRowStyle.BackColor;
+                        }
+                        else
+                        {
+                            cell.BackColor = grdrecord.RowStyle.BackColor;
+                        }
+                        cell.CssClass = "textmode";
+                    }
+                }
+
+                grdrecord.RenderControl(hw);
+
+                //style to format numbers to string
+                string style = @"<style> .textmode { } </style>";
+                Response.Write(style);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    protected void lnkExportToExcel_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            ExportGridToExcel();
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "validate", "javascript: alert('" + ex.Message + "');", true);
+        }
+    }
+
+    public override void VerifyRenderingInServerForm(Control control)
+    {
+        /* Verifies that the control is rendered */
+    }
+
+}
